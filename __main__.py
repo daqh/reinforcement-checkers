54e6a3d748932d391c4d172b2861e7d27eb09035
Valerio Di Pasquale <40913709+daqh@users.noreply.github.com>
2025-01-20 10:55:06 +0000

    Update training

--- __main__.py
+++ __main__.py
@@ -5,14 +5,17 @@ import time
 import random
 from prova_env import CheckersEnv
 
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, SAC
 
 def simulate_game():
     env = CheckersEnv()
 
-    model = PPO("MlpPolicy", env, verbose=1)
-    env.set_adversary(model)
+    ppo = PPO("MlpPolicy", env, verbose=1)
+    env.set_adversary(ppo)
+    ppo.learn(total_timesteps=10000)
 
-    model.learn(total_timesteps=10000)
+    sac = SAC("MlpPolicy", env, verbose=1)
+    env.set_adversary(ppo)
+    sac.learn(total_timesteps=10000)
 
 simulate_game()
