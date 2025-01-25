db8d4dc01eac0d29cd925c8c4a35f774c10a8bf4
Valerio Di Pasquale <valeriodipasq@gmail.com>
2025-01-25 14:20:03 +0100

    Update

--- board.py
+++ board.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class CheckersMove:
     def __init__(self, start, end):
         self.start = start
@@ -10,6 +12,7 @@ class CheckersMove:
 
     def __repr__(self):
         return f"({self.start} -> {self.end})"
+
 
 class CheckersBoard:
     def __init__(self):
@@ -54,6 +57,22 @@ class CheckersBoard:
             row_str = " ".join(str(cell) for cell in row)
             board_str += row_str + "\n"
         return board_str
+
+    def get_observation(self) -> np.ndarray:
+        # Aggiungere anche le mosse
+        observation = np.zeros((4, 8, 8), dtype=np.int8)
+        for row in range(8):
+            for col in range(8):
+                if self.board[row][col] == self.turn:
+                    observation[0, row, col] = self.turn
+                elif self.board[row][col] == self.turn * 2:
+                    observation[1, row, col] = self.turn
+                elif self.board[row][col] == -self.turn:
+                    observation[2, row, col] = -self.turn
+                elif self.board[row][col] == -self.turn * 2:
+                    observation[3, row, col] = -self.turn
+
+        return observation
     
     @property
     def turn(self) -> int:
