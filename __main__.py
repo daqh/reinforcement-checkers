30014823447468da042edd96fcad4a9c47807c61
Valerio Di Pasquale <valeriodipasq@gmail.com>
2025-01-25 14:36:24 +0100

    Update

--- __main__.py
+++ __main__.py
@@ -6,18 +6,20 @@ from prova_env import CheckersEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3 import PPO, SAC
 import matplotlib.pyplot as plt
+from copy import deepcopy
 
 def simulate_game():
     env_1 = CheckersEnv()
     env_2 = CheckersEnv()
 
     model_1 = PPO("MlpPolicy", env_1, verbose=1)
-    model_2 = SAC("MlpPolicy", env_2, verbose=1)
+    model_2 = SAC("MlpPolicy", env_2, verbose=1, device="mps")
     
     env_1.set_adversary(model_2)
     env_2.set_adversary(model_1)
-    
+
     for _ in range(5):
+
         print("Training model 1")
         model_1.learn(total_timesteps=1000, progress_bar=True)
         print("Training model 2")
