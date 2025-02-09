788b3ac7cf2b7aa43f1d03613f263e4d0327cb67
Valerio Di Pasquale <40913709+daqh@users.noreply.github.com>
2025-02-09 20:20:37 +0100

    Removed old env

--- models.py
+++ models.py
@@ -3,4 +3,4 @@ import numpy as np
 class RandomModel:
 
     def predict(*args):
-        return np.random.randint(0, 8, (4,), dtype=np.int32), 0
+        return np.random.randint(0, 64, (2,), dtype=np.int32), 0
